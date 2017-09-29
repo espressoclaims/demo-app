f95567369fdc3358cff2dc43fbb08b8778cdc758
NabaSadiaSiddiqui <naba.sadia.siddiqui@gmail.com>
2017-09-29 14:06:06 -0400

    Add a form to POST claims to blockchain network

--- app/provider/provider.js
+++ app/provider/provider.js
@@ -9,6 +9,35 @@ angular.module('myApp.provider', ['ngRoute'])
   });
 }])
 
-.controller('ProviderCtrl', [function() {
+.controller('ProviderCtrl', ['$scope', '$http', function($scope, $http) {
+  $scope.status = '';
 
+  $scope.isClaimable = function(service, amount) {
+    return amount < 100;
+  }
+
+  $scope.canClaim = function(service, amount) {
+    return 0.5*amount;
+  }
+
+  $scope.processClaim = function(claim) {
+    // FIXME: this logic is CRAP and needs to be replaced with an actual data source!!
+    if($scope.isClaimable(claim.servicePerformed, claim.amount)) {
+      claim.isClaimable = true;
+      claim.amount = $scope.canClaim(claim.servicePerformed, claim.amount);
+    } else {
+      claim.isClaimable = false;
+    }
+
+    $http({
+          url: 'http://localhost:8081/addClaim',
+          method: 'POST',
+          data: claim,
+          headers: {'Content-Type': 'application/json'}
+    }).then(function(response) {
+      $scope.status = response.data;
+    }, function(err) {
+      $scope.status = err.data;
+    });
+  };
 }]);
