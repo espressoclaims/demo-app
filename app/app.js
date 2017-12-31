a8245eb72d0885e5de524ac179f6de4e462dc3c5
NabaSadiaSiddiqui <naba.sadia.siddiqui@gmail.com>
2017-12-31 00:07:55 -0500

    Clean up

--- app/app.js
+++ app/app.js
@@ -3,8 +3,7 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
-  'myApp.transactions',
-  'myApp.version'
+  'myApp.transactions'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
