2935d472eace127ed8970cc159a5425095a25ba3
NabaSadiaSiddiqui <naba.sadia.siddiqui@gmail.com>
2018-01-06 01:33:28 -0500

    Use SSL

--- app/env.js
+++ app/env.js
@@ -2,7 +2,7 @@
   window.__env = window.__env || {};
 
   // API url
-  window.__env.apiUrl = 'http://35.196.165.21:80';
+  window.__env.apiUrl = 'https://35.196.165.21:80';
 
   // Whether or not to enable debug mode
   // Setting this to false will disable console output
