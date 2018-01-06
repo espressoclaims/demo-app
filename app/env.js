7e90f3cf5d7fbceee296055ecc8c35f10fefc44e
NabaSadiaSiddiqui <naba.sadia.siddiqui@gmail.com>
2018-01-06 01:31:37 -0500

    Update apiUrl to point to live instance of network

--- app/env.js
+++ app/env.js
@@ -2,10 +2,7 @@
   window.__env = window.__env || {};
 
   // API url
-  window.__env.apiUrl = 'http://localhost:8081';
-
-  // Base url
-  window.__env.baseUrl = '/';
+  window.__env.apiUrl = 'http://35.196.165.21:80';
 
   // Whether or not to enable debug mode
   // Setting this to false will disable console output
